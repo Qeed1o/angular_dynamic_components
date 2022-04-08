35f3c56bd637c27e9af65a6d61b790fbfc2a9f31
Qeed1o <sergeykavai@gmail.com>
2022-04-08 11:10:56 +0200

    Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentMap, FieldTypes } from './models';
 import { COMPONENTS_MAP_INJECTION_TOKEN, FIELDS_INJECTION_TOKEN, InitialFields } from './constants';
 import { ButtonComponent } from './components/button/button.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const map: ComponentMap = {
   [FieldTypes.Select]: FormselectComponent,
@@ -24,6 +28,10 @@ const map: ComponentMap = {
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
